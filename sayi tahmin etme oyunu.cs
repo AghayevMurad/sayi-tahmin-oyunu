6d72532414654bee99a7f5c1a0f75e71910e485f
Murad Aghayev <113592767+AghayevMurad@users.noreply.github.com>
2023-03-24 14:30:00 +0300

    Add files via upload

---
+++ sayi tahmin etme oyunu.cs
@@ -0,0 +1,75 @@
+﻿
+
+
+
+
+namespace sayi_tahmin
+{
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("**** Sayi tahmin etme oyunu ****");
+            Console.WriteLine();
+            Console.WriteLine("**** oyunun serteleri sayiyi 1 den basliyarak 2 2 atliyarak yaziniz ****");
+
+            Console.Write("1.ci sayi: ");
+            int i;
+            i = Convert.ToInt32(Console.ReadLine());
+            Console.Write("2.ci sayi: ");
+            Console.ReadLine();
+            Console.Write("3.cu sayi: ");
+            Console.ReadLine();
+            Console.Write("4.cu sayi: ");
+            Console.ReadLine();
+            Console.Write("5.ci sayi: ");
+            Console.ReadLine();
+
+
+            Console.WriteLine();
+            Console.WriteLine("Sizi tebrik ediyoruz Siz sayi tahmin etme oyununu kazandinz :) ");
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("**** Sayi tahmin etme oyunun cavablari ****");
+            Console.WriteLine();
+
+            for (i = 1; i < 10; i += 2)
+            {
+
+                Console.WriteLine(i);
+
+
+            }
+
+
+            Console.ReadLine();
+
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
